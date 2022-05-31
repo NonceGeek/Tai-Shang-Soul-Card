9b3d96b28e0b1ae3c7075a38ee14cfd743b68f25
Blockchain-Key <1416893908@qq.com>
2022-05-31 10:27:38 +0800

    modified home

--- dapp/namecard/src/utils/axiosUtils.js
+++ dapp/namecard/src/utils/axiosUtils.js
@@ -1,8 +1,8 @@
 import axios  from "axios";
 // 创建一个axios对象
 const newAxios = axios.create({
-    baseURL: "http://localhost:4000/", // local
-    // baseURL:"https://faasbyleeduckgo.gigalixirapp.com", // online
+    // baseURL: "http://localhost:4000/", // local
+    baseURL:"https://faasbyleeduckgo.gigalixirapp.com", // online
     headers: {
         'Content-Type': 'application/json;charset=UTF-8'
     },

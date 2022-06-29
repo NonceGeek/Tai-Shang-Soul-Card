2c7503353204d7917f5775c79fcbb37f2734046f
Blockchain-Key <1416893908@qq.com>
2022-06-29 14:31:55 +0800

    modified underscore naming and speedruns

--- dapp/namecard/src/pages/link/index.jsx
+++ dapp/namecard/src/pages/link/index.jsx
@@ -14,27 +14,56 @@ const web3Modal = new Web3Modal({
 });
 export default function index(props) {
   const [infos, setInfos] = useStorage("infos")
-  const [mirrorLink, setMirrorLink] = useState('')
-  const [githubLink, setGithubLink] = useState("")
-  const [designLink, setDesignLink] = useState("")
+  const [mirror_link, setMirrorLink] = useState(false)
+  const [github_link, setGithubLink] = useState("")
+  const [design_link, setDesignLink] = useState("")
+  const [ng,setNg] = useState(false)
+  const [offical,setOffical] = useState(false)
+  const [speedruns,setSpeedruns] = useState([])
   useEffect(()=>{
-    setInfos({...infos,mirrorLink,githubLink,designLink})
-  },[mirrorLink,githubLink,designLink])
+    setInfos({...infos,mirror_link,github_link,design_link,speedruns})
+  },[mirror_link,github_link,design_link,speedruns])
   const alChange = (event) => {
-    const mirrorLink = event.target.value
-    if(mirrorLink == 'true'){
+    const mirror_link = event.target.value
+    if(mirror_link == 'true'){
       setMirrorLink(false)
     }else{
       setMirrorLink(true)
     }
   }
+  useEffect(()=>{
+    const speedruns = []
+    if(ng){
+      speedruns.push('ng')
+    }
+    if(offical){
+      speedruns.push('offical')
+    }
+    setSpeedruns(speedruns)
+  },[ng,offical])
   const clChange = (event) => {
-    const githubLink = event.target.value
-    setGithubLink(githubLink)
+    const github_link = event.target.value
+    setGithubLink(github_link)
   }
   const dlChange = (event) => {
-    const designLink = event.target.value
-    setDesignLink(designLink)
+    const design_link = event.target.value
+    setDesignLink(design_link)
+  }
+  const ngChange = (event) => {
+    const ng = event.target.value
+    if(ng == 'true'){
+      setNg(false)
+    }else{
+      setNg(true)
+    }
+  }
+  const ofChange = (event) => {
+    const offical = event.target.value
+    if(offical == 'true'){
+      setOffical(false)
+    }else{
+      setOffical(true)
+    }
   }
   const addInfoByIPFS = async (infos) => {
     const info = JSON.stringify(infos)
@@ -67,28 +96,29 @@ export default function index(props) {
     }
   }
   const submitInfos = async () => {
-    const { _baseCache } = await addInfoByIPFS(infos)
-    const hash = _baseCache.get('z')
-    const { addr, msg, signature } = await signByMetamask()
-    const data = {
-      params: [
-        {ipfs: hash },
-        'user',
-        addr,
-        msg,
-        signature
-      ]
-    }
-    const res = await createUser(JSON.stringify(data))
-    if(res.data.result.status=='ok'){
-      props.history.push({
-        pathname:'/home',
-        query:{
-          address:infos.ethereum_addr,
-          role:'user'
-        }
-      });
-    }
+    console.log(infos);
+    // const { _baseCache } = await addInfoByIPFS(infos)
+    // const hash = _baseCache.get('z')
+    // const { addr, msg, signature } = await signByMetamask()
+    // const data = {
+    //   params: [
+    //     {ipfs: hash },
+    //     'user',
+    //     addr,
+    //     msg,
+    //     signature
+    //   ]
+    // }
+    // const res = await createUser(JSON.stringify(data))
+    // if(res.data.result.status=='ok'){
+    //   props.history.push({
+    //     pathname:'/home',
+    //     query:{
+    //       address:infos.ethereum_addr,
+    //       role:'user'
+    //     }
+    //   });
+    // }
   }
   return (
     <>
@@ -105,21 +135,30 @@ export default function index(props) {
         <div className={styles.form}>
           <div className={styles.formItem}>
             <p>Mirror Link</p>
-            <input type="checkbox" value={mirrorLink} onChange={alChange}/>
+            <input type="checkbox" value={mirror_link} onChange={alChange}/>
           </div>
           <div className={styles.formItem}>
             <p>Github Link</p>
-            <input type="text" value={githubLink} onChange={clChange} />
+            <input type="text" value={github_link} onChange={clChange} />
           </div>
           <div className={styles.formItem}>
             <p>Design Link</p>
-            <input type="text" value={designLink} onChange={dlChange} />
+            <input type="text" value={design_link} onChange={dlChange} />
+          </div>
+          <div className={styles.formItem}>
+            <p>speedruns</p>
+            <div className="flex justify-center items-center mb-5">
+              <input type="checkbox" id='ng' style={{width:'50px'}} value={ng} onChange={ngChange}/><label htmlFor="ng" style={{width:"100px"}}>ng</label>
+            </div>
+            <div className="flex justify-center items-center">
+              <input type="checkbox" id='offical'style={{width:'50px'}} value={offical} onChange={ofChange}/><label htmlFor="offical" style={{width:"100px"}}>offical</label> 
+            </div>
           </div>
         </div>
       </main>
       <footer>
         <button onClick={async() => {
-           await submitInfos(infos)
+           await submitInfos()
         }}>
           Upload to ipfs and submit
         </button>

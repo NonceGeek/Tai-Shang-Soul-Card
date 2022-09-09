f187f7156b56f5e2e5b79668bba075d0bb9e5826
Dream4ever <cnhwvista@gmail.com>
2022-09-09 22:49:48 +0800

    add tailwind-accent-color

--- dapp/tailwind.config.js
+++ dapp/tailwind.config.js
@@ -13,10 +13,8 @@ module.exports = {
       colors: {
         // 浅绿色渐变 - 炫彩2 - 起始
         'xc2-a': '#79D5A8',
-        'xc2-a/40': 'rgba(121, 213, 168, 0.4)',
         // 浅绿色渐变 - 炫彩2 - 结束
         'xc2-b': '#D5F97D',
-        'xc2-b/40': 'rgba(213, 249, 125, 0.4)',
         // 浅绿色渐变 - 炫彩（白） - 起始
         'xcb-a': '#53C78E',
         // 浅绿色渐变 - 炫彩（白） - 结束
@@ -35,5 +33,7 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('tailwind-accent-color')(),
+  ],
 };

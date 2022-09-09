671ae350bb83fd6e7e1242cd57a03201aa648ae6
Dream4ever <cnhwvista@gmail.com>
2022-09-09 17:06:03 +0800

    temp: landing page dialog

--- dapp/src/pages/landing/index.tsx
+++ dapp/src/pages/landing/index.tsx
@@ -1,11 +1,24 @@
-import './index.less'
+import { useState } from 'react'
+
 import Button from '@/components/Button'
 import Header from '@/components/Header'
 import GradientText from '@/components/GradientText'
 
+import './index.less'
+
 export default function IndexPage() {
+  const [showMask, setShowMask] = useState(false)
+
+  const toggleShowMask = () => {
+    setShowMask(!showMask)
+  }
+
+  const maskBlur = {
+    backdropFilter: 'blur(30px)',
+  }
+
   return (
-    <div className='page w-full min-h-screen bg-black text-white font-IBMPlexMono'>
+    <div className='page w-full min-h-screen relative bg-black text-white font-IBMPlexMono'>
       <div className="content w-main h-full mx-auto flex flex-col">
         <Header />
         <div className="slogon pt-40 flex flex-col items-end text-[56px] text-white font-bold">
@@ -16,7 +29,7 @@ export default function IndexPage() {
         <div className="vision w-full flex justify-end">
           <span className='mt-52 w-1/2 text-right'>We are providing identity verification to help break down social barriers and work to build a new relationship of mutual trust.</span>
         </div>
-        <div className="go mt-40 pb-20 flex justify-center">
+        <div className="go mt-40 pb-20 flex justify-center" onClick={() => toggleShowMask()}>
           <Button
             colorStyle="green"
             buttonText="Get Started"
@@ -25,6 +38,14 @@ export default function IndexPage() {
           />
         </div>
       </div>
+      {showMask && (
+        <div
+          className="mask absolute top-0 left-0 right-0 bottom-0 bg-black/30 flex justify-center items-center"
+          style={maskBlur}
+        >
+          <div className="dialog w-[422px] rounded">Hello World!</div>
+        </div>
+      )}
     </div>
   );
 }

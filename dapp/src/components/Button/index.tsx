f08f355c762b7b99686351813b0e49356b80db7b
Dream4ever <cnhwvista@gmail.com>
2022-09-09 22:50:01 +0800

    finish landing page

--- dapp/src/components/Button/index.tsx
+++ dapp/src/components/Button/index.tsx
@@ -1,12 +1,14 @@
 export interface Props {
-  colorStyle: 'white' | 'green';
-  buttonText: string;
-  font?: string;
-  fontSize?: 'lg';
+  colorStyle: 'white' | 'green',
+  buttonText: string,
+  wide?: boolean,
+  font?: string,
+  fontSize?: 'lg',
 }
 
-export default function index({ colorStyle, buttonText, fontSize, font = 'Inter' }: Props) {
+export default function index({ colorStyle, buttonText, wide, fontSize, font = 'Inter' }: Props) {
   return <span className={`px-6 py-2 rounded cursor-pointer font-bold
+      ${wide && 'px-0 w-52 inline-block text-center'}
       ${colorStyle === 'white' && 'border border-solid border-white text-white'}
       ${colorStyle === 'green' && 'bg-gradient-to-r from-xc2-a to-xc2-b text-black'}
       ${!fontSize && 'text-[20px]'}
--- dapp/src/pages/landing/index.tsx
+++ dapp/src/pages/landing/index.tsx
@@ -4,13 +4,36 @@ import Button from '@/components/Button'
 import Header from '@/components/Header'
 import GradientText from '@/components/GradientText'
 
+import CloseIcon from '@/assets/img/close-icon.png'
+import MetaMaskIcon from '@/assets/img/metamask.png'
+import OkIcon from '@/assets/img/ok.png'
+
 import './index.less'
+
+enum LoginStatus {
+  UnloggedIn = 'UnloggedIn',
+  Logging = 'Logging',
+  LoggedIn = 'LoggedIn',
+}
 
 export default function IndexPage() {
   const [showMask, setShowMask] = useState(false)
+  const [currLoginStatus, setCurrLoginStatus] = useState(LoginStatus.UnloggedIn)
 
   const toggleShowMask = () => {
     setShowMask(!showMask)
+  }
+
+  const hideMask = () => {
+    setShowMask(false)
+  }
+
+  const beginLogin = () => {
+    setCurrLoginStatus(LoginStatus.Logging)
+  }
+
+  const finishLogin = () => {
+    setCurrLoginStatus(LoginStatus.LoggedIn)
   }
 
   const maskBlur = {
@@ -40,10 +63,64 @@ export default function IndexPage() {
       </div>
       {showMask && (
         <div
-          className="mask absolute top-0 left-0 right-0 bottom-0 bg-black/30 flex justify-center items-center"
+          className="mask fixed top-0 left-0 right-0 bottom-0 bg-black/30 flex justify-center items-center"
           style={maskBlur}
         >
-          <div className="dialog w-[422px] rounded">Hello World!</div>
+          <div className="dialog-wrapper">
+            <div className="dialog w-[422px] bg-black p-4 flex flex-col items-center">
+              <div className="close-icon w-full flex justify-end items-center">
+                <img className='w-3 h-3 cursor-pointer' onClick={() => hideMask()} src={CloseIcon} alt="close" />
+              </div>
+              <div className='title mt-2 font-IBMPlexMonoBold text-[24px]'>{currLoginStatus !== LoginStatus.LoggedIn ? 'CONNECT TO WALLET' : 'SUCCESSFULLY CONNECTED'}</div>
+              {currLoginStatus === LoginStatus.UnloggedIn && (
+                <>
+                  <div className='metamask-wrapper mt-8'>
+                    <div className="metamask bg-black hover:bg-transparent py-1 pl-4 pr-6 flex items-center text-white hover:text-black cursor-pointer" onClick={() => beginLogin()}>
+                      <img className='w-8 h-8' src={MetaMaskIcon} alt="" />
+                      <div className='ml-4 font-IBMPlexMonoBold text-[14px]'>MetaMask</div>
+                    </div>
+                  </div>
+                  <div className="no-wallet mt-7 font-IBMPlexMono underline">I don't have a wallet</div>
+                  <div className='agree mt-5 flex items-center'>
+                    <input className='accent-green-300' type="checkbox" name="agree" id="agree" />
+                    <label htmlFor="agree" className='ml-2 opacity-50 font-IBMPlexMono text-[12px]'>I agree to Top5 Terms and Privacy Policy.</label>
+                  </div>
+                </>
+              )}
+              {currLoginStatus === LoginStatus.Logging && (
+                <>
+                  <div className="mt-3 px-9 font-IBMPlexMono text-[14px]">Skip approving every interaction with your wallet by allowing SoulCard to remember you.</div>
+                  <div className="address mt-5 px-1 py-2 bg-[#4a4a4a] opacity-50">
+                    <span className='font-Inter font-bold text-[#c5c5c5] text-[14px]'>9de3d8a...d7d2g6fe3e2vH1</span>
+                  </div>
+                  <div className='mt-6' onClick={() => finishLogin()}>
+                    <Button
+                      colorStyle="green"
+                      buttonText="Remember Me"
+                      wide={true}
+                      font="IBMPlexMonoBold"
+                    />
+                  </div>
+                  <div className="notice mt-7 px-9 font-IBMPlexMono text-[12px] text-white opacity-40">Signing keys can only sign messgaes and cannot hold funds. They are Stored securely in the browser database system.</div>
+                </>
+              )}
+              {currLoginStatus === LoginStatus.LoggedIn && (
+                <>
+                  <div className='mt-12'>
+                    <img className='w-20 mx-auto' src={OkIcon} alt="" />
+                  </div>
+                  <div className="mt-12 mb-12" onClick={() => hideMask()}>
+                    <Button
+                      colorStyle="green"
+                      buttonText="Start"
+                      wide={true}
+                      font="IBMPlexMonoBold"
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
         </div>
       )}
     </div>

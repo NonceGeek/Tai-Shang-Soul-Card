25e7d106f636c505efd1ed237a822611e29ba6f2
Dream4ever <cnhwvista@gmail.com>
2022-09-10 18:55:48 +0800

    finish about page

---
+++ dapp/src/components/Card/index.tsx
@@ -0,0 +1,17 @@
+export interface Props {
+  text: string,
+  width?: 'full',
+  height?: 'lg' | 'md' | 'sm',
+}
+
+export default function index({ text, width, height = 'sm' }: Props) {
+  return <div className={`flex-grow border border-[#79D5A880] border-solid hover:border-0 flex flex-col justify-center items-center font-IBMPlexMono text-[14px]
+      ${width === 'full' && 'w-full'}
+      ${height === 'sm' && 'h-[26px]'}
+      ${height === 'md' && 'h-[86px]'}
+      ${height === 'lg' && 'h-[142px]'}
+      bg-[#061417] hover:bg-gradient-to-r hover:from-xc2-a hover:to-xc2-b hover:text-black`}
+  >
+    {text.split('\n').map(str => <span key={str}>{str}</span>)}
+  </div>
+}
---
+++ dapp/src/components/GradientLine/index.tsx
@@ -0,0 +1,4 @@
+export default function index() {
+  return <span className='inline-block w-full h-[1.5px] bg-gradient-to-r from-xc2-a to-xc2-b'>
+  </span>
+}

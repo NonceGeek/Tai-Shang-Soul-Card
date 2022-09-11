5b97f7ce2d9b4f8c4fa877564a78f4304aab6aae
Huang,Karl (SrMD) BIIT-CN-S <karl.huang@boehringer-ingelheim.com>
2022-09-12 02:04:43 +0800

    完成dao样式

--- dapp/src/pages/personal/index.jsx
+++ dapp/src/pages/personal/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RightCard from '../../components/RightCard/index'
+import RightCardDao from '../../components/RightCardDao/index'
 
 const temp_data = {
   name: 'Robert Fox',
@@ -84,6 +85,132 @@ const temp_data = {
     }
   ]
 }
+const temp_data_dao = {
+  name: 'Dao Name',
+  dao_link: 'https://noncegeek.com/#/',
+  contract_address: 'contract address',
+  description: 'Our team is working on a decentralized social product in the Web3 environment.',
+  introduction: 'Have more than 6 years of Digital Product Design experience.',
+  social_links: {
+    twitter: "https://twitter.com/Web3dAppCamp",
+    mirror_link: "https://mirror.xyz/apecoder.eth",
+    github_link: "https://github.com/WeLightProject",
+    wechat: "197626581",
+    discord: "hitchhacker@3691"
+  },
+  location: 'California',
+  skills: ['Javascript', 'C++', 'Python', 'HTML', 'Node', 'C#', 'Java', 'Javascript', 'C++', 'Python', 'HTML', 'Node', 'C#', 'Java'],
+  awesome_things: [
+    {
+      project: 'Design for the transport',
+      link: 'www.baidu.com'
+    },
+    {
+      project: 'Probably One of The Most Common',
+      link: 'www.baidu.com'
+    },
+    {
+      project: 'LinkedIn Is No Longer LinkedIn Is No',
+      link: 'www.baidu.com'
+    }
+  ],
+  members: [
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: 'Co-founder',
+      is_core_member: true
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: '',
+      is_core_member: false
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: '',
+      is_core_member: false
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: '',
+      is_core_member: false
+    },
+    {
+      avator: 'member-avator',
+      name: 'Robert Fox',
+      position: '',
+      is_core_member: false
+    }
+  ],
+  partner: [
+    {
+      avator: 'member-avator',
+      name: 'NonceGeek DAO'
+    },
+    {
+      avator: 'member-avator',
+      name: 'NonceGeek DAO'
+    },
+    {
+      avator: 'member-avator',
+      name: 'NonceGeek DAO'
+    },
+    {
+      avator: 'member-avator',
+      name: 'NonceGeek DAO'
+    },
+    {
+      avator: 'member-avator',
+      name: 'NonceGeek DAO'
+    }
+  ]
+}
 export default function index() {
   return (
     <>
@@ -96,7 +223,8 @@ export default function index() {
               {/* edit component */}
             </div>
             <div className="w-2/5 h-screen p-54">
-              <RightCard data={temp_data} />
+              {/* <RightCard data={temp_data} /> */}
+              <RightCardDao data={temp_data_dao} />
             </div>
           </main>
         </div>

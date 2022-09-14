bf8433e5992fff4782179127ce42a7c9f03f87d7
Blockchain-Key <1416893908@qq.com>
2022-09-14 14:02:41 +0800

    update interface

--- dapp/src/requests/DataHandler.js
+++ dapp/src/requests/DataHandler.js
@@ -1,10 +1,18 @@
 import axios from '@/utils/axiosUtils';
 export const check_format = (data) =>
-  axios.post('/api/v1/run?name=DataHandler&func_name=check_format', data);
+  axios.post(
+    '/api/v1/run?name=SoulCard.DataHandler&func_name=check_format',
+    data,
+  );
 export const get_github_white_map = () =>
   axios.post(
-    '/api/v1/run?name=DataHandler&func_name=get_github_white_map',
+    '/api/v1/run?name=SoulCard.DataHandler&func_name=get_github_white_map',
     data,
   );
 export const analyze_github = (data) =>
-  axios.post('/api/v1/run?name=DataHandler&func_name=analyze_github', data);
+  axios.post(
+    '/api/v1/run?name=SoulCard.DataHandler&func_name=analyze_github',
+    data,
+  );
+export const rand_msg = (data) =>
+  axios.post('/api/v1/run?name=SoulCard.DataHandler&func_name=rand_msg', data);
--- dapp/src/requests/IpfsInteractor.js
+++ dapp/src/requests/IpfsInteractor.js
@@ -1,5 +1,11 @@
 import axios from '@/utils/axiosUtils';
 export const get_data = (data) =>
-  axios.post('/api/v1/run?name=IpfsInteractor&func_name=get_data', data);
+  axios.post(
+    '/api/v1/run?name=SoulCard.IpfsInteractor&func_name=get_data',
+    data,
+  );
 export const put_data = (data) =>
-  axios.post('/api/v1/run?name=IpfsInteractor&func_name=put_data', data);
+  axios.post(
+    '/api/v1/run?name=SoulCard.IpfsInteractor&func_name=put_data',
+    data,
+  );
